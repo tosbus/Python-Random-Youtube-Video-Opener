731ab7f32457ed6f74d5262d0aa11946b983d4ac
Warren <wbirkman@gmail.com>
2021-07-07 17:43:34 -0700

    Added README.md and main.py to the repo. README contains a description and requirements for main.py and main.py is just the primary file that is described

---
+++ main.py
@@ -0,0 +1,20 @@
+import random, webbrowser, requests, re
+from bs4 import BeautifulSoup as bs
+
+num = random.randint(1000, 10000)
+
+val = random.randint(0,2)
+
+if val == 2:
+	url = (f"https://www.youtube.com/results?search_query=IMG+0{num}")
+elif val == 1:
+	url = (f"https://www.youtube.com/results?search_query=MOV+0{num}")
+else:
+	url = (f"https://www.youtube.com/results?search_query=MP4+0{num}")
+
+webbrowser.open(url)
+
+page = requests.get(url)
+website = str(bs(page.text, 'lxml'))
+links = re.findall(r"/watch\?v=\S\S\S\S\S\S\S\S\S\S\S",website)
+webbrowser.open(f"https://www.youtube.com/{links[random.randint(0,len(links))]}")
