6b8042ea57309e75e7ae3489af91dd08db86bf67
Warren <wbirkman@gmail.com>
2021-07-11 21:53:47 -0700

    fixed bug that was causing lnks not to open

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import random, webbrowser, requests, re
 from bs4 import BeautifulSoup as bs
 from randomsearch import search
 
-num = random.randint(1000, 10000)
+def trysearch():
+	try:
+		f = random.randint(1000, 10000)
 
-val = random.randint(0,2)
+		val = random.randint(0,2)
 
-if val == 2:
-	url = (f"https://www.youtube.com/results?search_query={search()}")
-elif val == 1:
-	url = (f"https://www.youtube.com/results?search_query=MOV+0{num}")
-else:
-	url = (f"https://www.youtube.com/results?search_query=MP4+0{num}")
+		if val == 2:
+			url = (f"https://www.youtube.com/results?search_query={search()}")
+		elif val == 1:
+			url = (f"https://www.youtube.com/results?search_query=MOV+0{f}")
+		else:
+			url = (f"https://www.youtube.com/results?search_query=MP4+0{f}")
 
-# To open search results
-#webbrowser.open(url)
+		#To open search results
+		#webbrowser.open(url)
 
-page = requests.get(url)
-website = str(bs(page.text, 'lxml'))
-links = re.findall(r"/watch\?v=\S\S\S\S\S\S\S\S\S\S\S",website)
-webbrowser.open(f"https://www.youtube.com/{links[random.randint(0,len(links))]}")
+		page = requests.get(url)
+		website = str(bs(page.text, 'lxml'))
+		links = re.findall(r"/watch\?v=\S\S\S\S\S\S\S\S\S\S\S",website)
+		webbrowser.open(f"https://www.youtube.com/{links[random.randint(0,len(links))]}")
+	except:
+		return False
+
+while (trysearch() == False):
+	trysearch()
